70acae95507786f49d9c46899a71cf56f82d0fb7
fuubi <fuubi@protonmail.ch>
2024-09-04 11:41:46 +0200

    add ai service

--- preparation/infra/network/main.tf
+++ preparation/infra/network/main.tf
@@ -1,15 +1,15 @@
 variable "AZURE_SUBSCRIPTION_ID" {
-  default     = ""
+  default = ""
 }
 
 terraform {
-required_providers {
+  required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
     }
-}
+  }
 
-   
+
 }
 provider "azurerm" {
   subscription_id = var.AZURE_SUBSCRIPTION_ID
@@ -19,7 +19,7 @@ provider "azurerm" {
 # Resource group
 resource "azurerm_resource_group" "hackfestival_rg" {
   name     = "hackfestival-resources"
-  location = "Sweden Central"  # Set location to Sweden Central
+  location = "Sweden Central" # Set location to Sweden Central
 }
 
 # Virtual Network
@@ -36,6 +36,7 @@ resource "azurerm_subnet" "hackfestival_subnet" {
   resource_group_name  = azurerm_resource_group.hackfestival_rg.name
   virtual_network_name = azurerm_virtual_network.hackfestival_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+  service_endpoints = ["Microsoft.CognitiveServices"]
 }
 
 # Network Security Group (optional)
@@ -70,6 +71,29 @@ resource "azurerm_search_service" "hackfestival_search" {
     azurerm_subnet.hackfestival_subnet
   ]
 }
+
+# Azure AI Services Account
+resource "azurerm_ai_services" "hackfestival_ai" {
+   name                = "hackfestival-ai"
+   resource_group_name = azurerm_resource_group.hackfestival_rg.name
+   location            = azurerm_resource_group.hackfestival_rg.location
+   sku_name            = "S0"
+   custom_subdomain_name = "hackfestival-ai"
+   fqdns = []
+
+   network_acls {
+     default_action = "Deny"
+     ip_rules = []
+
+     virtual_network_rules {
+       ignore_missing_vnet_service_endpoint = false
+       subnet_id = azurerm_subnet.hackfestival_subnet.id
+     }
+   }
+
+}
+
+
 
 # Output the Search Service name
 output "search_service_name" {
