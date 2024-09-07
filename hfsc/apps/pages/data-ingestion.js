9cc30a43dc18f894882a794ff3cbd0a702bfcff7
Fuubi <fuubi@protonmail.ch>
2024-09-07 15:50:01 +0000

    drop that

--- hfsc/apps/pages/data-ingestion.js
+++ hfsc/apps/pages/data-ingestion.js
@@ -155,27 +155,3 @@ customElements.define('data-ingestion-page', DataIngestionPage);
 
 
 
-class PageTwo extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
-        <ion-header>
-          <ion-toolbar>
-            <ion-buttons slot="start">
-              <ion-back-button></ion-back-button>
-            </ion-buttons>
-            <ion-title>Page Two</ion-title>
-          </ion-toolbar>
-        </ion-header>
-        <ion-content class="ion-padding">
-          <h1>Page Two</h1>
-          <div>
-            <ion-nav-link router-direction="forward" component="page-three">
-              <ion-button>Go to Page Three</ion-button>
-            </ion-nav-link>
-          </div>
-        </ion-content>
-      `;
-  }
-}
-
-customElements.define('page-two', PageTwo);
